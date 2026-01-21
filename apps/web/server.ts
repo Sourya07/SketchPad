35be559052a8ad909887c9cc0f1c7b08953100fa
Sourya07 <singhsourya137@gmail.com>
2026-01-21 20:56:40 +0530

    added the ws server

--- apps/web/server.ts
+++ apps/web/server.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://sketchpad-9wud.onrender.com"
-export const WEBSOCKET = process.env.NEXT_PUBLIC_WS_URL || "ws//sketchpad-1.onrender.com"
+export const WEBSOCKET = process.env.NEXT_PUBLIC_WS_URL || "wss://sketchpad-1.onrender.com"
